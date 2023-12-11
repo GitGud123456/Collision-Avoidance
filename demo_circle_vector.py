1ba890bdbd765bb262f492e56a9b980e0558d57b
GitGud123456 <98549374+GitGud123456@users.noreply.github.com>
2023-12-11 14:53:36 -0700

    Update demo_circle_vector.py

--- demo_circle_vector.py
+++ demo_circle_vector.py
@@ -5,51 +5,49 @@ from pygame.locals import *
 import math
 
 
+BLACK = (0,0,0)
 
 class Vector:
 
-    def __init__(self, x, y):
-
-        self.x = x
-        self.y = y
-
+    def __init__(self, *args):
+        if len(args) == 2:
+            self.x = float(args[0])
+            self.y = float(args[1])
+        elif len(args) == 1 and isinstance(args[0],Vector): # make a copy of a INPUTED Vector
+            other = args[0]
+            self.x = float(other.x)
+            self.y = float(other.y)
 
 
     def __add__(self, other):
-
         return Vector(self.x + other.x, self.y + other.y)
 
-
-
     def __mul__(self, scalar):
-        if isinstance(scalar, (int, float)):
+        if isinstance(scalar, (int,float)):
             return Vector(self.x * scalar, self.y * scalar)
-        elif isinstance(scalar, Vector):
-            return Vector(self.x * scalar.x, self.y * scalar.y)
         else:
             raise TypeError("Unsupported operand type for multiplication.")
     
     def __truediv__(self,scalar):
         return Vector(self.x / scalar, self.y /scalar)
 
+    def draw(self, screen):
+        radius = 2
+        distance = math.sqrt((old.x - self.x)**2 + (old.y - self.y)**2)
+        if distance > 2*radius:
+            pygame.draw.circle(screen, BLACK, (int(self.x), int(self.y)), radius)
 
 
 class Circle:
 
-    def __init__(self, position, velocity, radius, color):
-
-        self.position = Vector(*position)
-
+    def __init__(self, position, velocity, radius, color,mass):
+        self.position = position
         self.velocity = Vector(*velocity)
-
         self.radius = radius
-
+        self.mass = mass
         self.color = color
 
-
-
     def move(self):
-
         self.position = self.position + self.velocity
 
 
@@ -57,13 +55,9 @@ class Circle:
     def check_wall_collision(self, width, height):
 
         if self.position.x - self.radius <= 0 or self.position.x + self.radius >= width:
-
             self.velocity.x *= -1
 
-
-
         if self.position.y - self.radius <= 0 or self.position.y + self.radius >= height:
-
             self.velocity.y *= -1
 
 
@@ -80,9 +74,9 @@ def perfectly_elastic_collision(circle1, circle2):
 
     v2 = circle2.velocity
 
-    m1 = 1  # Mass of circle1 (assumed to be 1 for simplicity)
+    m1 = 1  # Mass of circle1 
 
-    m2 = 1  # Mass of circle2 (assumed to be 1 for simplicity)
+    m2 = 1  # Mass of circle2
 
 
 
@@ -95,6 +89,34 @@ def perfectly_elastic_collision(circle1, circle2):
     circle1.velocity = new_v1
 
     circle2.velocity = new_v2
+
+
+def elastic_collision(obj1, obj2):
+    # Calculate relative velocity components
+    relative_velocity_x = obj2.vx - obj1.vx
+    relative_velocity_y = obj2.vy - obj1.vy
+
+    # Calculate relative velocity in terms of the normal vector
+    relative_velocity_normal = (
+        relative_velocity_x * (obj2.x_pos - obj1.x_pos) +
+        relative_velocity_y * (obj2.y_pos - obj1.y_pos)
+    ) / ((obj2.x_pos - obj1.x_pos)**2 + (obj2.y_pos - obj1.y_pos)**2)
+
+    # Calculate new velocities after collision using the normal and tangential components
+    obj1.vx += relative_velocity_normal * (obj2.x_pos - obj1.x_pos)
+    obj1.vy += relative_velocity_normal * (obj2.y_pos - obj1.y_pos)
+
+    obj2.vx -= relative_velocity_normal * (obj2.x_pos - obj1.x_pos)
+    obj2.vy -= relative_velocity_normal * (obj2.y_pos - obj1.y_pos)
+
+
+
+
+
+
+
+
+
 
 
 
@@ -160,8 +182,11 @@ while running:
 
     # Update circle positions based on velocities
 
-    circle1.move()
 
+    
+
+
+    circle1.move()
     circle2.move()
 
 
@@ -169,7 +194,6 @@ while running:
     # Check for collisions with the walls
 
     circle1.check_wall_collision(WIDTH, HEIGHT)
-
     circle2.check_wall_collision(WIDTH, HEIGHT)
 
 
@@ -177,9 +201,7 @@ while running:
     # Check for collision between circles
 
     distance = math.sqrt((circle1.position.x - circle2.position.x)**2 + (circle1.position.y - circle2.position.y)**2)
-
     if distance <= 2 * circle1.radius:
-
         perfectly_elastic_collision(circle1, circle2)
 
 
